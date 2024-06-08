cee9c5dfd9d54e8885ae701699979c69bf2f617a
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-06-08 23:40:40 +0200

    Add HTML & JS human selection

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const computerSelection = document.getElementById("computer-selection")
 const computerButton = document.getElementById("computer-button")
+const humanSelection = document.getElementById("human-selection")
+const humanButtons = document.querySelectorAll(".human-button")
+
 
 
 function getComputerChoice() {
@@ -9,3 +12,12 @@ function getComputerChoice() {
     }
 
 computerButton.addEventListener("click", getComputerChoice)
+
+function selectedOption(event) {
+    const clickedButton = event.target.textContent
+    humanSelection.textContent = "You selected " + clickedButton
+}
+
+humanButtons.forEach(button => {
+    button.addEventListener("click", selectedOption)
+})
